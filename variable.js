cf6b57ae5f26d3303e9798643a379949ec11a08d
Abadie Arthur <arz54@live.fr>
2023-04-05 10:37:04 +0200

    pane

--- variable.js
+++ variable.js
@@ -32,12 +32,12 @@ var baselayers = {
 
 //catégorie_piscicole=1 et categorie_piscicole=2
 var categorie1 = water.features.filter(x => x.properties.categorie_piscicole == "1");
-var mapcategorie1 = L.geoJSON(categorie1, { style: { color: "#2fff00" } })
+var mapcategorie1 = L.geoJSON(categorie1, { style: { color: "#2fff00",weight:'2' } })
 var categorie2 = water.features.filter(x => x.properties.categorie_piscicole == "2");
-var mapcategorie2 = L.geoJSON(categorie2, { style: { color: "#006eff" } })
+var mapcategorie2 = L.geoJSON(categorie2, { style: { color: "#006eff",weight:'2'} })
 //reserve de pêche
 var reserve_peche = water.features.filter(x => x.properties.fishing_restriction == "no_fishing");
-var reserve_peche = L.geoJSON(reserve_peche, { style: { color: "#e30909" } })
+var reserve_peche = L.geoJSON(reserve_peche, { style: { color: "#e30909",weight:'2',pane: 'shadowPane' } })
 var reserve_tempo = water.features.filter(x => x.properties.fishing_restriction == "temporary_no_fishing");
-var reserve_tempo = L.geoJSON(reserve_tempo, { style: { color: "#e88c23" } })
+var reserve_tempo = L.geoJSON(reserve_tempo, { style: { color: "#e88c23",weight:'2',pane: 'shadowPane'} })
 
