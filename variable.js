c601b2120e48f98cacee6656804272c610659895
Abadie Arthur <arz54@live.fr>
2023-04-05 18:59:25 +0200

    pop up

--- variable.js
+++ variable.js
@@ -37,7 +37,7 @@ var categorie2 = water.features.filter(x => x.properties.categorie_piscicole == 
 var mapcategorie2 = L.geoJSON(categorie2, { style: { color: "#006eff",weight:'2'} })
 //reserve de pêche
 var reserve_peche = water.features.filter(x => x.properties.fishing_restriction == "no_fishing");
-var reserve_peche = L.geoJSON(reserve_peche, { style: { color: "#e30909",weight:'2',pane: 'shadowPane' } })
+var reserve_peche = L.geoJSON(reserve_peche, { style: { color: "#e30909",weight:'2',pane: 'shadowPane'} })
 var reserve_tempo = water.features.filter(x => x.properties.fishing_restriction == "temporary_no_fishing");
 var reserve_tempo = L.geoJSON(reserve_tempo, { style: { color: "#e88c23",weight:'2',pane: 'shadowPane'} })
 
