97ca727545cea71fb5d9cae18a904e5971ca39df
Abadie Arthur <arz54@live.fr>
2023-04-07 14:53:37 +0200

    popup

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,48 @@
 function popupcourseau(feature, layer) {
-    var popupContent = '<table>\
-                    <tr>\
-                        <td colspan="2"><strong>name</strong><br />' + (feature.properties['name']) + '</td>\
-                    </tr>\
-                    <tr>\
-                        <td colspan="2"><strong>ref:sandre</strong><br />' + (feature.properties['ref_sandre']) + '</td>\
-                    </tr>\
-                </table>';
-
+    var popupContent = 
+    '<table>\
+        <tr>\
+            <td> <strong>Nom</strong><br />' + (feature.properties['name']) + '</td>\
+        </tr>\
+        <tr>\
+            <td> <strong>Code sandre</strong><br />' + (feature.properties['ref_sandre']) + '</td>\
+        </tr>\
+    </table>';
         layer.bindPopup(popupContent);
-    };
+        };
+
+function popupreserve(feature, layer) {
+    var popupContentReserve = 
+    '<table>\
+        <tr>\
+            <td> <strong>Nom</strong><br />' + (feature.properties['name']) + '</td>\
+        </tr>\
+        <tr>\
+            <td> <strong>Code sandre</strong><br />' + (feature.properties['ref_sandre']) + '</td>\
+        </tr>\
+        <tr>\
+            <td> <strong></strong><br />' + (feature.properties['fishing:fr:description']) + '</td>\
+        </tr>\
+        <tr>\
+            <td> <strong></strong><br />' + (feature.properties['fishing:fr:name']) + '</td>\
+        </tr>\
+        <tr>\
+            <td> <strong>réference</strong><br />' + (feature.properties['fishing:fr:source:pref:arrete']) + '</td>\
+        </tr>\
+    </table>';
+        layer.bindPopup(popupContentReserve);
+        };
+
+
+function popupcategoriepiscicole(feature, layer) {
+    var popupContentcategoriepiscicole = 
+    '<table>\
+        <tr>\
+            <td> <strong>Nom</strong><br />' + (feature.properties['name']) + '</td>\
+        </tr>\
+        <tr>\
+            <td> <strong>Code sandre</strong><br />' + (feature.properties['ref_sandre']) + '</td>\
+        </tr>\
+    </table>';
+        layer.bindPopup(popupContentcategoriepiscicole);
+        };
--- js/variable.js
+++ js/variable.js
@@ -1,26 +1,21 @@
+var reserveoverlay = L.layerGroup();
+var categoriepiscicoleoverlay = L.layerGroup();
+
 //categorie piscicole
 var categoriepiscicole = L.geoJSON(json_categorie_piscicole, {
     style: function(feature) {
         switch (feature.properties.categorie_piscicole) {
             case '1': return {color: "#2fff00", weight: '2'};
-            case '2':   return {color: "#006eff", weight: '2'};
+            case '2': return {color: "#006eff", weight: '2'};
         }
-    }
-});
+    },onEachFeature :popupcategoriepiscicole}
+    ).addTo(categoriepiscicoleoverlay);
 //réserve de peche
 var reservepeche = L.geoJSON(json_fishing_restriction, {
     style: function(feature) {
         switch (feature.properties.fishing_restriction) {
             case 'temporary_no_fishing': return {color: "#e88c23",weight:'2',pane: 'shadowPane'};
-            case 'no_fishing':   return {color: "#e30909",weight:'2',pane: 'shadowPane'};
+            case 'no_fishing': return {color: "#e30909",weight:'2',pane: 'shadowPane',};
         }
-    }
-});
-
-var reserveoverlay = L.layerGroup();
-var categoriepiscicoleoverlay = L.layerGroup();
-
-var overlays = {
-    'réserve de pêche': reserveoverlay,
-    'catégorie piscicole' : categoriepiscicoleoverlay
-};
+    },onEachFeature :popupreserve}
+    ).addTo(reserveoverlay);
