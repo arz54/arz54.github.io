f1627f87857fa3b776d9a2796f613dd0f70ad4d6
Abadie Arthur <arz54@live.fr>
2023-04-07 13:18:22 +0200

    c ciao Autolinker

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,12 @@
-var autolinker = new Autolinker({truncate: {length: 30, location: 'smart'}});
-
-
 function popupcourseau(feature, layer) {
     var popupContent = '<table>\
                     <tr>\
-                        <td colspan="2"><strong>name</strong><br />' + (feature.properties['name'] !== null ? autolinker.link(feature.properties['name'].toLocaleString()) : '') + '</td>\
+                        <td colspan="2"><strong>name</strong><br />' + (feature.properties['name']) + '</td>\
                     </tr>\
                     <tr>\
-                        <td colspan="2"><strong>ref:sandre</strong><br />' + (feature.properties['ref_sandre'] !== null ? autolinker.link(feature.properties['ref_sandre'].toLocaleString()) : '') + '</td>\
+                        <td colspan="2"><strong>ref:sandre</strong><br />' + (feature.properties['ref_sandre']) + '</td>\
                     </tr>\
                 </table>';
 
         layer.bindPopup(popupContent);
-    }
+    };
